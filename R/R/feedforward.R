4bb6f80b12313f853515c04d2ead04d5a204ebf2
Matías Roodschild <mroodschild@gmail.com>
2018-10-16 18:07:52 -0300

    handle Loss Functions

--- R/R/feedforward.R
+++ R/R/feedforward.R
@@ -7,7 +7,7 @@
 #'
 #' @return a Java instance of SpellCorrector
 #' @export
-getFeedforward<-function() {
+sequencial_model<-function() {
   .jaddLibrary('froog', 'inst/java/froog-0.4.jar')
   .jaddClassPath('inst/java/froog-0.4,jar')
   net <- .jnew('org/gitia/froog/r/rFeedforward')
@@ -22,38 +22,76 @@ addLayer<-function(net, input, neurons, activation){
   .jcall(net, 'V','addLayer', as.integer(input), as.integer(neurons), activation)
 }
 
+#' @param net
+#' @param matrix
 out<-function(net, matrix){
   a <- .jcall(net, '[D','out', matrix, as.integer(nrow(matrix)), as.integer(ncol(matrix)))
   nrowOutput <- .jcall(net,'I','getOutCount')
-  m <- matrix(data = a, nrow = nrowOutput, ncol = as.integer(ncol(matrix)),byrow = TRUE)
+  m <- matrix(data = a, nrow = nrowOutput, ncol = as.integer(ncol(matrix)), byrow = TRUE)
   return(m)
 }
 
-summary<-function(net){
-  a <- .jcall(net, 'S','summary')
-  return(a)
-}
-
-bp<-function(net, input, output, epochs = 100, acceleration = "", acc_param = 0.9){
-  .jcall(net, 'V','bp', input, as.integer(nrow(input)), as.integer(ncol(input)),
-         output, as.integer(nrow(output)), as.integer(ncol(output)), as.integer(epochs),
-         acceleration, acc_param)
-}
-
-sgd<-function(net, input, output, epochs = 100, batch_size = 10, acceleration = "", acc_param = 0.9){
-  .jcall(net, 'V','sgd', input, as.integer(nrow(input)), as.integer(ncol(input)),
-         output, as.integer(nrow(output)), as.integer(ncol(output)), as.integer(epochs),
-         as.integer(batch_size), acceleration, acc_param)
-}
-
-cg<-function(net, input, output, epochs = 100, beta_rule = "fletcher_reeves"){
-  .jcall(net, 'V','cg', input, as.integer(nrow(input)), as.integer(ncol(input)),
+#' @param net
+#' @param input
+#' @param output
+#' @param epochs
+#' @param loss_function
+#' @param acceleration
+#' @param acc_param
+#' @param accuracy
+bp<-function(net, input, output, epochs = 100, loss_function ="rmse", acceleration = "", acc_param = 0.9, accuracy = FALSE){
+  .jcall(net, 'V','bp',
+         input, as.integer(nrow(input)), as.integer(ncol(input)),
          output, as.integer(nrow(output)), as.integer(ncol(output)),
-         as.integer(epochs), beta_rule)
+         as.integer(epochs),
+         loss_function,
+         acceleration,
+         acc_param,
+         as.logical(accuracy))
 }
 
+#' @param net
+#' @param input
+#' @param output
+#' @param epochs
+#' @param batch_size
+#' @param loss_function
+#' @param acceleration
+#' @param acc_param
+#' @param accuracy
+sgd<-function(net, input, output, epochs = 100, batch_size = 10, loss_function ="rmse", acceleration = "", acc_param = 0.9, accuracy = FALSE){
+  .jcall(net, 'V','sgd',
+         input, as.integer(nrow(input)), as.integer(ncol(input)),
+         output, as.integer(nrow(output)), as.integer(ncol(output)),
+         as.integer(epochs),
+         as.integer(batch_size),
+         loss_function,
+         acceleration,
+         acc_param,
+         accuracy)
+}
+
+#' @param net
+#' @param input
+#' @param output
+#' @param epochs
+#' @param beta_rule
+cg<-function(net, input, output, epochs = 100, beta_rule = "fletcher_reeves"){
+  .jcall(net, 'V','cg',
+         input, as.integer(nrow(input)), as.integer(ncol(input)),
+         output, as.integer(nrow(output)), as.integer(ncol(output)),
+         as.integer(epochs),
+         beta_rule)
+}
+
+#' @param net
+#' @param input
+#' @param output
+#' @param epochs
 scg<-function(net, input, output, epochs = 100){
-  .jcall(net, 'V','scg', input, as.integer(nrow(input)), as.integer(ncol(input)),
+  .jcall(net, 'V','scg',
+         input, as.integer(nrow(input)), as.integer(ncol(input)),
          output, as.integer(nrow(output)), as.integer(ncol(output)),
          as.integer(epochs))
 }
+
