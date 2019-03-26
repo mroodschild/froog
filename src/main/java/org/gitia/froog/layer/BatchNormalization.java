e51a4cdd5b3e80e58fed77a52a15465130644629
Matías Roodschild <mroodschild@gmail.com>
2019-03-26 00:16:22 -0300

    BatchNormalization

--- src/main/java/org/gitia/froog/layer/BatchNormalization.java
+++ src/main/java/org/gitia/froog/layer/BatchNormalization.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 
+ * Copyright 2019
  *   Matías Roodschild <mroodschild@gmail.com>.
  *   Jorge Gotay Sardiñas <jgotay57@gmail.com>.
  *   Adrian Will <adrian.will.01@gmail.com>.
@@ -19,9 +19,9 @@
  */
 package org.gitia.froog.layer;
 
-import org.gitia.froog.transferfunction.FunctionFactory;
-import org.gitia.froog.transferfunction.TransferFunction;
 import java.util.Random;
+import java.util.stream.IntStream;
+import org.ejml.data.MatrixType;
 import org.ejml.simple.SimpleMatrix;
 import org.gitia.froog.layer.initialization.WeightInit;
 
@@ -31,9 +31,8 @@ import org.gitia.froog.layer.initialization.WeightInit;
  */
 public class BatchNormalization implements Layer {
 
-    protected SimpleMatrix Gamma;//W[neuronas x entrada]
+    protected SimpleMatrix gamma;//W[neuronas x 1]
     protected SimpleMatrix Beta;//B[neuronas x 1]
-    protected TransferFunction function;
 
     protected Random random = new Random();
 
@@ -52,12 +51,11 @@ public class BatchNormalization implements Layer {
      *
      * @param input number of inputs
      * @param output number of neurons
-     * @param funcion transfer function
      * @param WeightInit
      * @param random
      */
-    public BatchNormalization(int input, int output, String funcion, String WeightInit, Random random) {
-        this(input, output, funcion, WeightInit, 0, random);
+    public BatchNormalization(int input, int output, String WeightInit, Random random) {
+        this(input, output, WeightInit, 0, random);
     }
 
     /**
@@ -69,16 +67,14 @@ public class BatchNormalization implements Layer {
      *
      * @param input number of inputs
      * @param output number of neurons
-     * @param funcion transfer function
      * @param WeightInit
      * @param keepProb for dropout
      * @param random
      */
-    public BatchNormalization(int input, int output, String funcion, String WeightInit, double keepProb, Random random) {
+    public BatchNormalization(int input, int output, String WeightInit, double keepProb, Random random) {
         this.random = random;
-        this.Gamma = new SimpleMatrix(output, input);
+        this.gamma = new SimpleMatrix(output, input);
         this.Beta = new SimpleMatrix(output, 1);
-        this.function = FunctionFactory.getFunction(funcion);
     }
 
     /**
@@ -90,11 +86,10 @@ public class BatchNormalization implements Layer {
      *
      * @param input cantidad de entradas
      * @param output cantidad de neuronas
-     * @param funcion función usadas en las neuronas
      * @param random heredado
      */
-    public BatchNormalization(int input, int output, String funcion, Random random) {
-        this(input, output, funcion, WeightInit.DEFAULT, random);
+    public BatchNormalization(int input, int output, Random random) {
+        this(input, output, WeightInit.DEFAULT, random);
     }
 
     /**
@@ -106,22 +101,19 @@ public class BatchNormalization implements Layer {
      *
      * @param input
      * @param output
-     * @param funcion
      */
-    public BatchNormalization(int input, int output, String funcion) {
-        this(input, output, funcion, new Random());
+    public BatchNormalization(int input, int output) {
+        this(input, output, new Random());
     }
 
     /**
      *
      * @param W
      * @param B
-     * @param funcion
      */
-    public BatchNormalization(SimpleMatrix W, SimpleMatrix B, String funcion) {
-        this.Gamma = W;
+    public BatchNormalization(SimpleMatrix W, SimpleMatrix B) {
+        this.gamma = W;
         this.Beta = B;
-        this.function = FunctionFactory.getFunction(funcion);
     }
 
     /**
@@ -143,17 +135,31 @@ public class BatchNormalization implements Layer {
      */
     @Override
     public SimpleMatrix output(SimpleMatrix a) {
+        
+        if (gamma == null) {
+            initGamma(a);
+        }
+        
+
         return null;
     }
-
-    /**
-     * This function implements the Droupout regularization
-     *
-     * @param a input to the layer
-     * @return return outputs after doing dropuot
-     */
-    public SimpleMatrix outputDropout(SimpleMatrix a) {
-        return null;
+    
+    public void initGamma(SimpleMatrix a){
+        int row = a.numRows();
+        int cols = a.numCols();
+        if (gamma == null) {
+            gamma = new SimpleMatrix(row, 1, MatrixType.DDRM);
+        }
+        IntStream.range(0, row).parallel()
+                .forEach(i -> {
+                    int idx = i * cols;
+                    double sum = 0;
+                    for (int j = 0; j < cols; j++) {
+                        //aux.set(idx, aux.get(idx)+B.get(i));
+                        sum += a.get(idx++);
+                    }
+                    gamma.set(i, sum / (double) cols);
+                });
     }
 
     /**
@@ -162,42 +168,7 @@ public class BatchNormalization implements Layer {
      * @return
      */
     public int numNeuron() {
-        return Gamma.numRows();
-    }
-
-    /**
-     * las filas son las neuronas y las columnas son los pesos de cada una.<br>
-     * <br>
-     * W[neuronas x entrada] = cantidad de pesos en la capa
-     *
-     * @return
-     */
-    public SimpleMatrix getW() {
-        return Gamma;
-    }
-
-    /**
-     *
-     * @param W
-     */
-    public void setW(SimpleMatrix W) {
-        this.Gamma = W;
-    }
-
-    /**
-     *
-     * @return
-     */
-    public SimpleMatrix getB() {
-        return Beta;
-    }
-
-    /**
-     *
-     * @param B
-     */
-    public void setB(SimpleMatrix B) {
-        this.Beta = B;
+        return gamma.numRows();
     }
 
     /**
@@ -215,30 +186,4 @@ public class BatchNormalization implements Layer {
     public void setRandom(Random random) {
         this.random = random;
     }
-
-    /**
-     *
-     * @return
-     */
-    public TransferFunction getFunction() {
-        return function;
-    }
-
-    /**
-     *
-     * @param function
-     */
-    public void setFunction(TransferFunction function) {
-        this.function = function;
-    }
-
-    /**
-     *
-     * @return
-     */
-    @Override
-    public String toString() {
-        return function.toString();
-    }
-
 }
