2ffa6792309b35f7e027f31ade364620c4cb7d84
Matías Roodschild <mroodschild@gmail.com>
2021-10-13 16:09:42 -0300

    EJML 0.41

--- src/main/java/org/gitia/froog/transferfunction/Z.java
+++ src/main/java/org/gitia/froog/transferfunction/Z.java
@@ -16,6 +16,9 @@
 package org.gitia.froog.transferfunction;
 
 import java.util.stream.IntStream;
+
+import org.ejml.dense.row.CommonOps_MT_DDRM;
+import org.ejml.ops.CommonOps_BDRM;
 import org.ejml.simple.SimpleMatrix;
 
 /**
@@ -25,8 +28,12 @@ import org.ejml.simple.SimpleMatrix;
 public class Z {
 
     public static SimpleMatrix output(SimpleMatrix W, SimpleMatrix a, SimpleMatrix B) {
-        SimpleMatrix aux = W.mult(a);
+        //SimpleMatrix aux = W.mult(a);
         //CommonOps_DDRM.multAdd(a, b, c);
+
+        SimpleMatrix aux = new SimpleMatrix(W.numRows(), a.numCols());
+        CommonOps_MT_DDRM.mult(W.getDDRM(), a.getDDRM(), aux.getDDRM());
+        
         int cols = aux.numCols();
 //        IntStream.range(0, size).parallel()
 //                .forEach(i -> aux.setColumn(i, 0, aux.extractVector(false, i).plus(B).getDDRM().getData()));
